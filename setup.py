1709c327520fefb7321b509f7f8c1b997ebfee48
Edoardo Putti <edoardo.putti@gmail.com>
2018-01-20 16:24:36 +0100

    add python package setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='bookmark-archiver',
+    version='0.0.3',
+    description='Your own personal Way-Back Machine',
+    author='Nick Sweeting',
+    project_urls={
+        'website': 'https://pirate.github.io/bookmark-archiver/',
+        'repository': 'https://github.com/pirate/bookmark-archiver',
+    },
+    license='MIT',
+    packages=[
+        'bookmark_archiver',
+    ],
+    install_requires=[],
+    scripts=[
+        'bin/archive',
+    ],
+    zip_safe=False
+)
