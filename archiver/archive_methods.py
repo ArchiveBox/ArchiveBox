a59d609571b787ae4fc02c261cf52152e9aa820c
William Esz <flagaello@gmail.com>
2018-10-15 13:09:31 +0200

    Fix archive_dot_org submit_url
    It was removing functional query parameters. (e.g., https://news.ycombinator.com/item?id=18216459)

--- archiver/archive_methods.py
+++ archiver/archive_methods.py
@@ -352,7 +352,7 @@ def archive_dot_org(link_dir, link, timeout=TIMEOUT):
         archive_org_url = open(path, 'r').read().strip()
         return {'output': archive_org_url, 'status': 'skipped'}
 
-    submit_url = 'https://web.archive.org/save/{}'.format(link['url'].split('?', 1)[0])
+    submit_url = 'https://web.archive.org/save/{}'.format(link['url'])
 
     success = False
     CMD = ['curl', '-L', '-I', '-X', 'GET', submit_url]
