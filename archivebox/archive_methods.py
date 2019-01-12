911b84a79b43dabdb189056bb96e873fd0f4c6e7
Nick Sweeting <git@nicksweeting.com>
2019-01-11 22:13:51 -0500

    make wget cli options easier to read

--- archivebox/archive_methods.py
+++ archivebox/archive_methods.py
@@ -210,8 +210,18 @@ def fetch_wget(link_dir, link, requisites=FETCH_WGET_REQUISITES, timeout=TIMEOUT
 
     CMD = [
         # WGET CLI Docs: https://www.gnu.org/software/wget/manual/wget.html
-        *'wget -N -E -np -x -H -k -K -S --restrict-file-names=unix'.split(' '),
-        *(('-p',) if FETCH_WGET_REQUISITES else ()),
+        'wget',
+        # '--server-response',
+        '--no-verbose',
+        '--timestamping',
+        '--adjust-extension',
+        '--convert-links',
+        '--force-directories',
+        '--backup-converted',
+        '--span-hosts',
+        '--no-parent',
+        '--restrict-file-names=unix',
+        *(('--page-requisites',) if FETCH_WGET_REQUISITES else ()),
         *(('--user-agent="{}"'.format(WGET_USER_AGENT),) if WGET_USER_AGENT else ()),
         *((() if CHECK_SSL_VALIDITY else ('--no-check-certificate',))),
         link['url'],
