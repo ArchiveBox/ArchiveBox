20de4515158e99ead79471ff9780ef6b87c8ed68
Nick Sweeting <git@nicksweeting.com>
2019-01-23 01:48:04 -0500

    add linux config example

--- archivebox/config.py
+++ archivebox/config.py
@@ -60,10 +60,11 @@ if not CHROME_BINARY:
         'chromium-browser',
         'chromium',
         'google-chrome',
+        'google-chrome-stable',
+        'google-chrome-unstable',
         'google-chrome-beta',
         'google-chrome-canary',
         'google-chrome-dev',
-        'google-chrome-unstable',
     )
     for name in common_chrome_executable_names:
         full_path_exists = shutil.which(name)
