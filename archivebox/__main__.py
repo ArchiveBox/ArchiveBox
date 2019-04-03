749f06fe5b49251e82ed53350ad1afbd5b9281da
Nick Sweeting <git@nicksweeting.com>
2019-04-03 00:29:58 -0400

    simplify bin shortcut

--- archivebox/__main__.py
+++ archivebox/__main__.py
@@ -2,16 +2,9 @@
 
 __package__ = 'archivebox'
 
-
-import os
-import sys
-
-PYTHON_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(PYTHON_DIR)
-
 from .cli.archivebox import main
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
 
