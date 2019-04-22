29ced7b5c85fba071aa38109b8396e13df6b1258
Nick Sweeting <git@nicksweeting.com>
2019-04-22 13:36:27 -0400

    allow running archivebox core commands from manage.py

--- archivebox/core/management/commands/archivebox.py
+++ archivebox/core/management/commands/archivebox.py
@@ -1,11 +1,18 @@
+__package__ = 'archivebox'
+
 from django.core.management.base import BaseCommand
 
 
-from legacy.archive import main
+from .cli import run_subcommand
 
 
 class Command(BaseCommand):
-    help = 'ArchiveBox test.bee'
+    help = 'Run an ArchiveBox CLI subcommand (e.g. add, remove, list, etc)'
+
+    def add_arguments(self, parser):
+        parser.add_argument('subcommand', type=str, help='The subcommand you want to run')
+        parser.add_argument('command_args', nargs='*', help='Arguments to pass to the subcommand')
+
 
     def handle(self, *args, **kwargs):
-        main(*args)
+        run_subcommand(kwargs['subcommand'], args=kwargs['command_args'])
