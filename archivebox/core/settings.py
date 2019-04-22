834aaa159101082dc36227541f5e6005732bf2e3
Nick Sweeting <git@nicksweeting.com>
2019-04-22 19:07:39 -0400

    better template staticfile management with themes dir

--- archivebox/core/settings.py
+++ archivebox/core/settings.py
@@ -1,26 +1,34 @@
 __package__ = 'archivebox.core'
 
 import os
+import sys
 
 SECRET_KEY = '---------------- not a valid secret key ! ----------------'
 DEBUG = True
+ALLOWED_HOSTS = ['*']
 
+REPO_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), os.path.pardir, os.path.pardir))
 OUTPUT_DIR = os.path.abspath(os.getenv('OUTPUT_DIR', os.curdir))
 DATABASE_FILE = os.path.join(OUTPUT_DIR, 'index.sqlite3')
 
+ACTIVE_THEME = 'default'
+
+IS_SHELL = 'shell' in sys.argv[:3] or 'shell_plus' in sys.argv[:3]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    # 'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.admin',
     'django.contrib.staticfiles',
 
     'core',
 
     'django_extensions',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -29,14 +37,18 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['themes'],
+        'DIRS': [
+            os.path.join(REPO_DIR, 'themes', ACTIVE_THEME),
+            os.path.join(REPO_DIR, 'themes', 'default'),
+            os.path.join(REPO_DIR, 'themes'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -58,12 +70,38 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
+
+################################################################################
+### Security Settings
+################################################################################
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_DOMAIN = None
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_COOKIE_AGE = 1209600  # 2 weeks
+LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/'
+PASSWORD_RESET_URL = '/accounts/password_reset/'
+
+
+SHELL_PLUS = 'ipython'
+SHELL_PLUS_PRINT_SQL = False
+IPYTHON_ARGUMENTS = ['--no-confirm-exit', '--no-banner']
+IPYTHON_KERNEL_DISPLAY_NAME = 'ArchiveBox Django Shell'
+if IS_SHELL:
+    os.environ['PYTHONSTARTUP'] = os.path.join(REPO_DIR, 'core', 'welcome_message.py')
 
 
 LANGUAGE_CODE = 'en-us'
@@ -73,4 +111,15 @@ USE_L10N = True
 USE_TZ = False
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(REPO_DIR, 'themes', ACTIVE_THEME, 'static'),
+    os.path.join(REPO_DIR, 'themes', 'default', 'static'),
+    os.path.join(REPO_DIR, 'themes', 'static'),
+]
+
+SERVE_STATIC = True
+
+
--- archivebox/core/urls.py
+++ archivebox/core/urls.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from django.urls import path
+from django.utils.translation import ugettext_lazy
 
+from django.urls import path, include
+from django.conf import settings
 
-from core.views import MainIndex, LinkDetails
+from core.views import MainIndex, AddLinks, LinkDetails
+
+admin.site.site_header = 'ArchiveBox Admin'
+admin.site.index_title = 'Archive Administration'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('archive/<timestamp>/', LinkDetails.as_view(), name='LinkDetails'),
-    path('main/', MainIndex.as_view(), name='Home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('add/', AddLinks.as_view(), name='AddLinks'),
+    path('', MainIndex.as_view(), name='Home'),
 ]
+
+
+if settings.SERVE_STATIC:
+    # serve staticfiles via runserver
+    from django.contrib.staticfiles import views
+    urlpatterns += [
+        path('static/<path>', views.serve),
+    ]
