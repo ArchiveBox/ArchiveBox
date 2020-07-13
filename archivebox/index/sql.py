4c4b1e6a4bde5edb9e11942245a21437e73fe6df
Nick Sweeting <git@sweeting.me>
2020-07-13 11:33:35 -0400

    fix link creation

--- archivebox/index/sql.py
+++ archivebox/index/sql.py
@@ -29,7 +29,7 @@ def write_sql_main_index(links: List[Link], out_dir: str=OUTPUT_DIR) -> None:
     with transaction.atomic():
         for link in links:
             info = {k: v for k, v in link._asdict().items() if k in Snapshot.keys}
-            Snapshot.objects.update_or_create(url=url, defaults=info)
+            Snapshot.objects.update_or_create(url=link.url, defaults=info)
 
 @enforce_types
 def write_sql_link_details(link: Link, out_dir: str=OUTPUT_DIR) -> None:
