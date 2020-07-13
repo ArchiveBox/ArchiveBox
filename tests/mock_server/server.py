8f2543fa61b5406e0b058e415f5c9309d832decf
Cristian <cristian@swapps.com>
2020-07-13 09:39:43 -0500

    test: Refactor args tests to use local webserver instead of remote requests

--- tests/mock_server/server.py
+++ tests/mock_server/server.py
@@ -1,8 +1,17 @@
-from bottle import route, run
+from os.path import abspath
+from os import getcwd
+from pathlib import Path
 
-@route('/')
+from bottle import route, run, static_file
+
+@route("/")
 def index():
     return "Hello"
+
+@route("/static/<filename>")
+def static_path(filename):
+    template_path = abspath(getcwd()) / Path("tests/mock_server/templates")
+    return static_file(filename, root=template_path)
 
 def start():
     run(host='localhost', port=8080)
