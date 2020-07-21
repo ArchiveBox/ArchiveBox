b9c03f50949c679282bb7d637bff156bac96a2dc
Nick Sweeting <git@sweeting.me>
2020-07-21 16:24:43 -0400

    shellcheck fixes

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # detect userid:groupid of contents of data folder
 DATA_DIR="${DATA_DIR:-/data}"
@@ -7,7 +7,7 @@ ARCHIVEBOX_USER="${ARCHIVEBOX_USER:-archivebox}"
 # Autodetect UID and GID of host user based on ownership of files in the volume
 USID=$(stat --format="%u" "$DATA_DIR")
 GRID=$(stat --format="%g" "$DATA_DIR")
-COMMAND="$@"
+COMMAND="$*"
 
 # run django as the host user's uid:gid so that any files touched have the same permissions as outside the container
 # e.g. ./manage.py runserver
@@ -15,8 +15,8 @@ COMMAND="$@"
 chown "$USID":"$GRID" "$DATA_DIR"
 
 if [[ "$USID" != 0 && "$GRID" != 0 ]]; then
-    usermod -u $USID $ARCHIVEBOX_USER
-    groupmod -g $GRID $ARCHIVEBOX_USER
+    usermod -u "$USID" "$ARCHIVEBOX_USER"
+    groupmod -g "$GRID" "$ARCHIVEBOX_USER"
     chown -R "$USID":"$GRID" "/home/$ARCHIVEBOX_USER"
 fi
-gosu $ARCHIVEBOX_USER bash -c "$COMMAND"
+gosu "$ARCHIVEBOX_USER" bash -c "$COMMAND"
