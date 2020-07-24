82f8f8b661aab3badcab258c03b8081a34608558
Cristian <cristian@swapps.com>
2020-07-24 10:34:47 -0500

    fix: Use config information for path instead of hardcoded values

--- archivebox/index/schema.py
+++ archivebox/index/schema.py
@@ -12,6 +12,8 @@ from dataclasses import dataclass, asdict, field, fields
 
 from ..system import get_dir_size
 
+from ..config import OUTPUT_DIR, ARCHIVE_DIR_NAME
+
 class ArchiveError(Exception):
     def __init__(self, message, hints=None):
         super().__init__(message)
@@ -76,7 +78,7 @@ class ArchiveResult:
                 info['start_ts'] = parse_date(info['start_ts'])
                 info['end_ts'] = parse_date(info['end_ts'])
             if "pwd" not in keys:
-                info["pwd"] = str(os.getcwd() / Path(f"archive/{json_info['timestamp']}"))
+                info["pwd"] = str(Path(OUTPUT_DIR) / ARCHIVE_DIR_NAME / json_info["timestamp"])
             if "cmd_version" not in keys:
                 info["cmd_version"] = "Undefined"
             if "cmd" not in keys:
