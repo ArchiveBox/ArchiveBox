0aea5ed3e82294c0584a147eae14f40fefda45de
Nick Sweeting <git@sweeting.me>
2021-02-01 14:37:34 -0500

    fix handling of skipped ArchiveResult entries with null output

--- archivebox/core/migrations/0007_archiveresult.py
+++ archivebox/core/migrations/0007_archiveresult.py
@@ -37,7 +37,7 @@ def forwards_func(apps, schema_editor):
         for extractor in history:
             for result in history[extractor]:
                 ArchiveResult.objects.create(extractor=extractor, snapshot=snapshot, cmd=result["cmd"], cmd_version=result["cmd_version"] or 'unknown', 
-                start_ts=result["start_ts"], end_ts=result["end_ts"], status=result["status"], pwd=result["pwd"], output=result["output"])
+                start_ts=result["start_ts"], end_ts=result["end_ts"], status=result["status"], pwd=result["pwd"], output=result["output"] or 'null')
 
 
 def verify_json_index_integrity(snapshot):
