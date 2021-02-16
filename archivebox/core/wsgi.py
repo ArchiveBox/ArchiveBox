87069dd41e4e488a6086d3bb9e3868dbf4baa7f4
Nick Sweeting <git@sweeting.me>
2021-02-16 04:16:51 -0500

    fix wsgi.py

--- archivebox/core/wsgi.py
+++ archivebox/core/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from archivebox.config import setup_django
+setup_django(in_memory_db=False, check_db=True)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archivebox.settings')
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
