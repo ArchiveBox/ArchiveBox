4223801a04fac1bdaf05f92d3a27582d0a1e4796
Srevin Saju <srevinsaju@sugarlabs.org>
2021-05-07 13:51:19 +0300

    fix: attempt to fix ValueError in AppConfig
    ValueError: Primary key 'django.db.models.UUIDField' referred by core.apps.CoreConfig.default_auto_field must subclass AutoField.

--- archivebox/core/apps.py
+++ archivebox/core/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
-    default_auto_field = 'django.db.models.UUIDField'
+    default_auto_field = 'django.db.models.BigAutoField'
