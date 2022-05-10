0b4df768bac61d246a0e13397095e451e5af2887
Nick Sweeting <git@sweeting.me>
2022-05-09 23:50:56 -0700

    hack to check for generator type cause too lazy to import

--- archivebox/logging_util.py
+++ archivebox/logging_util.py
@@ -432,7 +432,7 @@ def log_archive_method_finished(result: "ArchiveResult"):
         # Prettify error output hints string and limit to five lines
         hints = getattr(result.output, 'hints', None) or ()
         if hints:
-            if isinstance(hints, (list, tuple)):
+            if isinstance(hints, (list, tuple, type(_ for _ in ()))):
                 hints = (hint.decode() for hint in hints if isinstance(hint, bytes))
             else:
                 if isinstance(hints, bytes):
