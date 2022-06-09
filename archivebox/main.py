5509b5cd8be09660473ddc2986e8f0ffeff354e3
Nick Sweeting <git@sweeting.me>
2022-06-08 19:08:33 -0700

    Update main.py

--- archivebox/main.py
+++ archivebox/main.py
@@ -230,8 +230,8 @@ def version(quiet: bool=False,
             f'DEBUG={DEBUG}',
             f'IS_TTY={IS_TTY}',
             f'TZ={TIMEZONE}',
-            f'DB=django.db.backends.sqlite3 (({CONFIG["SQLITE_JOURNAL_MODE"]})',
-            f'FS={"remote" if fs_is_mount else "local"} {"atomic" if ENFORCE_ATOMIC_WRITES else "non-atomic"} {PUID}:{PGID} ({OUTPUT_PERMISSIONS})',
+            #f'DB=django.db.backends.sqlite3 (({CONFIG["SQLITE_JOURNAL_MODE"]})',  # add this if we have more useful info to show eventually
+            f'FS={"remote" if fs_is_mount else "local"} {"atomic" if ENFORCE_ATOMIC_WRITES else "non-atomic"} {PUID}:{PGID};({OUTPUT_PERMISSIONS})',
             f'SEARCH_BACKEND_ENGINE={SEARCH_BACKEND_ENGINE}',
         )
         print()
