27d5d1ddc86b35e9d5a74ffe5a197e9de276a9c9
Nick Sweeting <github@sweeting.me>
2024-05-06 23:13:52 -0700

    revert queryset intersection back to union for search results

--- archivebox/core/mixins.py
+++ archivebox/core/mixins.py
@@ -13,7 +13,7 @@ class SearchResultsAdminMixin:
             return qs.distinct(), use_distinct
         try:
             qsearch = query_search_index(search_term)
-            qs = qs & qsearch
+            qs = qs | qsearch
         except Exception as err:
             print(f'[!] Error while using search backend: {err.__class__.__name__} {err}')
             messages.add_message(request, messages.WARNING, f'Error from the search backend, only showing results from default admin search fields - Error: {err}')
