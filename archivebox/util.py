6c4f3fc83aa8a935e9c501d6c8dbe1354db9f93d
Nick Sweeting <github@sweeting.me>
2024-08-26 20:15:36 -0700

    fix chrome headless=new arg

--- archivebox/util.py
+++ archivebox/util.py
@@ -304,11 +304,7 @@ def chrome_args(**options) -> List[str]:
     cmd_args += CHROME_EXTRA_ARGS
 
     if options['CHROME_HEADLESS']:
-        chrome_major_version = int(re.search(r'\s(\d+)\.\d', CHROME_VERSION)[1])
-        if chrome_major_version >= 111:
-            cmd_args += ("--headless=new",)
-        else:
-            cmd_args += ('--headless',)
+        cmd_args += ("--headless=new",)   # expects chrome version >= 111
 
     if not options['CHROME_SANDBOX']:
         # assume this means we are running inside a docker container
