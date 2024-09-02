3ea116f0b622dd297a04752a5cd2f7c5a02dc6dc
Nick Sweeting <github@sweeting.me>
2024-09-02 01:14:41 -0700

    tweak Pkgs app name and admin button CSS

--- archivebox/pkg/apps.py
+++ archivebox/pkg/apps.py
@@ -4,8 +4,10 @@ from django.apps import AppConfig
 
 
 class PkgsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'pkg'
+    verbose_name = 'Package Management'
+    
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         from .settings import LOADED_DEPENDENCIES
