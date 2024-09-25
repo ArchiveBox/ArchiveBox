bde0bf82635c7f591d6840773978d72408b39c35
Nick Sweeting <github@sweeting.me>
2024-09-24 19:37:05 -0700

    load ipython rich extension in archivebox shell

--- archivebox/core/shell_welcome_message.py
+++ archivebox/core/shell_welcome_message.py
@@ -2,4 +2,9 @@ from archivebox.logging_util import log_shell_welcome_msg
 
 
 if __name__ == '__main__':
+    # load the rich extension for ipython for pretty printing
+    # https://rich.readthedocs.io/en/stable/introduction.html#ipython-extension
+    get_ipython().run_line_magic('load_ext', 'rich')
+    
+    # print the welcome message
     log_shell_welcome_msg()
