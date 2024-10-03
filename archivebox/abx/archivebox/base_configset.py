9241a45bb8bb1ebce04fdd9031c2524cea06a337
Nick Sweeting <git@sweeting.me>
2024-10-03 11:42:27 -0400

    Update base_configset.py

--- archivebox/abx/archivebox/base_configset.py
+++ archivebox/abx/archivebox/base_configset.py
@@ -204,24 +204,10 @@ class ArchiveBoxBaseConfig(BaseSettings):
 class BaseConfigSet(ArchiveBoxBaseConfig, BaseHook):      # type: ignore[type-arg]
     hook_type: ClassVar[HookType] = 'CONFIG'
 
-    # def register(self, settings, parent_plugin=None):
-    #     # self._plugin = parent_plugin                                      # for debugging only, never rely on this!
-
-    #     settings.FLAT_CONFIG = benedict(getattr(settings, "FLAT_CONFIG", {}))
-    #     # pass FLAT_CONFIG so far into our config model to load it
-    #     loaded_config = self
-    #     # then dump our parsed config back into FLAT_CONFIG for the next plugin to use
-    #     settings.FLAT_CONFIG.merge(loaded_config.model_dump(include=set(self.model_fields.keys())))
-        
-    #     settings.REGISTERED_CONFIGS = getattr(settings, "REGISTERED_CONFIGS", None) or benedict({})
-    #     settings.REGISTERED_CONFIGS[self.id] = self
-    #     self._original_id = id(self)
-
-    #     super().register(settings, parent_plugin=parent_plugin)
-
+    # @abx.hookimpl
     # def ready(self, settings):
-    #     # reload config from environment, in case it's been changed by any other plugins
-    #     self.__init__()
+    #    # reload config from environment, in case it's been changed by any other plugins
+    #    self.__init__()
 
 
     @abx.hookimpl
