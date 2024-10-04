3f1a19dd354528edd6f270961e1f14c853b38569
Nick Sweeting <git@sweeting.me>
2024-10-03 18:24:57 -0700

    fix type imports

--- archivebox/abx/archivebox/use.py
+++ archivebox/abx/archivebox/use.py
@@ -26,14 +26,14 @@ def get_PLUGINS():
         for plugin in pm.get_plugins()
     })
 
-def get_HOOKS(PLUGINS) -> Dict[str, BaseHook]:
+def get_HOOKS(PLUGINS) -> Dict[str, 'BaseHook']:
     return benedict({
         hook.id: hook
         for plugin in PLUGINS.values()
             for hook in plugin.hooks
     })
 
-def get_CONFIGS() -> Dict[str, BaseConfigSet]:
+def get_CONFIGS() -> Dict[str, 'BaseConfigSet']:
     return benedict({
         config_id: config
         for plugin_configs in pm.hook.get_CONFIGS()
@@ -47,7 +47,7 @@ def get_FLAT_CONFIG() -> Dict[str, Any]:
             for key, value in plugin_config_dict.items()
     })
 
-def get_BINPROVIDERS() -> Dict[str, BaseBinProvider]:
+def get_BINPROVIDERS() -> Dict[str, 'BaseBinProvider']:
     # TODO: move these to plugins
     from abx.archivebox.base_binary import apt, brew, env
     builtin_binproviders = [apt, brew, env]
@@ -58,49 +58,49 @@ def get_BINPROVIDERS() -> Dict[str, BaseBinProvider]:
             for binprovider in plugin_binproviders
     })
 
-def get_BINARIES() -> Dict[str, BaseBinary]:
+def get_BINARIES() -> Dict[str, 'BaseBinary']:
     return benedict({
         binary.id: binary
         for plugin_binaries in pm.hook.get_BINARIES()
             for binary in plugin_binaries
     })
 
-def get_EXTRACTORS() -> Dict[str, BaseExtractor]:
+def get_EXTRACTORS() -> Dict[str, 'BaseExtractor']:
     return benedict({
         extractor.id: extractor
         for plugin_extractors in pm.hook.get_EXTRACTORS()
             for extractor in plugin_extractors
     })
 
-def get_REPLAYERS() -> Dict[str, BaseReplayer]:
+def get_REPLAYERS() -> Dict[str, 'BaseReplayer']:
     return benedict({
         replayer.id: replayer
         for plugin_replayers in pm.hook.get_REPLAYERS()
             for replayer in plugin_replayers
     })
 
-def get_CHECKS() -> Dict[str, BaseCheck]:
+def get_CHECKS() -> Dict[str, 'BaseCheck']:
     return benedict({
         check.id: check
         for plugin_checks in pm.hook.get_CHECKS()
             for check in plugin_checks
     })
 
-def get_ADMINDATAVIEWS() -> Dict[str, BaseAdminDataView]:
+def get_ADMINDATAVIEWS() -> Dict[str, 'BaseAdminDataView']:
     return benedict({
         admin_dataview.id: admin_dataview
         for plugin_admin_dataviews in pm.hook.get_ADMINDATAVIEWS()
             for admin_dataview in plugin_admin_dataviews
     })
 
-def get_QUEUES() -> Dict[str, BaseQueue]:
+def get_QUEUES() -> Dict[str, 'BaseQueue']:
     return benedict({
         queue.id: queue
         for plugin_queues in pm.hook.get_QUEUES()
             for queue in plugin_queues
     })
 
-def get_SEARCHBACKENDS() -> Dict[str, BaseSearchBackend]:
+def get_SEARCHBACKENDS() -> Dict[str, 'BaseSearchBackend']:
     return benedict({
         searchbackend.id: searchbackend
         for plugin_searchbackends in pm.hook.get_SEARCHBACKENDS()
