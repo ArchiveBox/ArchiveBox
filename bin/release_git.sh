b1264a6f8be2934d3805dcd0b39ea477ee3ac4ce
Nick Sweeting <github@sweeting.me>
2024-10-05 03:20:56 -0700

    force push tags

--- bin/release_git.sh
+++ bin/release_git.sh
@@ -18,6 +18,6 @@ cd "$REPO_DIR"
 # Push build to github
 echo "[^] Pushing release commit + tag to Github"
 git tag -a "v$VERSION" -m "v$VERSION"
-git push origin --tags
+git push origin -f --tags
 echo "    To finish publishing the release go here:"
 echo "        https://github.com/ArchiveBox/ArchiveBox/releases/new"
