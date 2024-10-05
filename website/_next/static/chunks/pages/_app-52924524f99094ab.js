9778eff8e49ebe7a0d867e9e44beae5a5f6de9bf
Nick Sweeting <git@sweeting.me>
2024-10-05 07:03:04 -0700

    add new static site

---
+++ website/_next/static/chunks/pages/_app-52924524f99094ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{1597:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(6530)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(1597),_(1247)}),_N_E=n.O()}]);
---
+++ website/_next/static/fHwRXOSkIeBcHyyaHSOnq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c92d5c4bb2b49926.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
