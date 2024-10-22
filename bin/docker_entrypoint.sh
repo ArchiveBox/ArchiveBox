e2081e3d6744d47f2cb5b0615fa8acf6de478cb2
Nick Sweeting <github@sweeting.me>
2024-10-22 02:25:37 -0700

    chown lib dir tmp dir in docker entrypoint

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -113,8 +113,10 @@ fi
 # also create and chown tmp dir and lib dirs
 mkdir -p "$DATA_DIR"/lib/bin
 chown $PUID:$PGID "$DATA_DIR"/lib "$DATA_DIR"/lib/*
+chown $PUID:$PGID "$LIB_DIR/*" 2>/dev/null &
 mkdir -p "$DATA_DIR"/tmp/workers
 chown $PUID:$PGID "$DATA_DIR"/tmp "$DATA_DIR"/tmp/*
+chown $PUID:$PGID "$TMP_DIR/*" 2>/dev/null &
 
 # (this check is written in blood in 2023, QEMU silently breaks things in ways that are not obvious)
 export IN_QEMU="$(pmap 1 | grep qemu >/dev/null && echo 'True' || echo 'False')"
