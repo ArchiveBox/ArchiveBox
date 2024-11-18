b852442efcc8230bf410eb485bf5402c5d74e3ee
Nick Sweeting <github@sweeting.me>
2024-11-17 22:57:20 -0800

    add crawls app back to django admin

--- archivebox/api/v1_api.py
+++ archivebox/api/v1_api.py
@@ -39,6 +39,7 @@ html_description=f'''
 def register_urls(api: NinjaAPI) -> NinjaAPI:
     api.add_router('/auth/',     'api.v1_auth.router')
     api.add_router('/core/',     'api.v1_core.router')
+    api.add_router('/crawls/',   'api.v1_core.router')
     api.add_router('/cli/',      'api.v1_cli.router')
     api.add_router('/jobs/',     'api.v1_actors.router')
     return api
--- archivebox/queues/apps.py
+++ archivebox/queues/apps.py
@@ -1,14 +1,7 @@
 from django.apps import AppConfig
 
-import abx
-
 
 class QueuesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'queues'
 
-
-@abx.hookimpl
-def register_admin(admin_site):
-    from queues.admin import register_admin
-    register_admin(admin_site)
